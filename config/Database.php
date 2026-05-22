a7d8cee75204256cf80e332ebe79bdc6c4b6d13e
sarafasra <sarafasraoui75@gmail.com>
2026-05-22 10:39:51 +0100

    implémentation du singleton pour la connexion PDO

--- config/Database.php
+++ config/Database.php
@@ -7,7 +7,7 @@ class Database {
     private static ?PDO $instance = null;
 
     public static function getInstance(): PDO {
-
+ 
         if (self::$instance === null) {
 
             self::$instance = new PDO(
